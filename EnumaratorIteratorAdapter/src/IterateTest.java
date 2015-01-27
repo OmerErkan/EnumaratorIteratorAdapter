d3b2ad175c0228b4bb75023863502a0772d0f6e6
omer erkan <omererkanomeroglu@gmail.com>
2015-01-27 15:05:17 +0200

    commit2..

--- EnumaratorIteratorAdapter/src/IterateTest.java
+++ EnumaratorIteratorAdapter/src/IterateTest.java
@@ -5,6 +5,7 @@ public interface IterateTest
 	public void hasMoreElements();
 	public void nextElement();
 	public void remove();
+	//
 //	public static void main(String[] args) {
 //		Vector<String> v = new Vector<String>();
 //		v.add("one");		
