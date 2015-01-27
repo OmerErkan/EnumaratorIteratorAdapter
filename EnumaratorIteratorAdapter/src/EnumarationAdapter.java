7f56de276aa53cd70e74cd57bd60a9e8d4b11734
omer erkan <omererkanomeroglu@gmail.com>
2015-01-27 10:41:50 +0200

    initial check in

---
+++ EnumaratorIteratorAdapter/src/EnumarationAdapter.java
@@ -0,0 +1,20 @@
+public class EnumarationAdapter implements EnumerateTest , IterateTest {
+	
+	public OldStyleList<String> osl;
+
+	public void hasMoreElements() {
+
+	}
+
+	public void nextElement() {
+
+	}
+
+	public void remove() {
+		
+		
+	}
+
+	
+
+}
---
+++ EnumaratorIteratorAdapter/src/IterateTest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public interface IterateTest 
+{
+	public void hasMoreElements();
+	public void nextElement();
+	public void remove();
+//	public static void main(String[] args) {
+//		Vector<String> v = new Vector<String>();
+//		v.add("one");		
+//		v.add("two");		
+//		v.add("three");		
+//		v.add("four");		
+//
+//		// the recommended technique ....
+//		Iterator i = v.iterator();
+//
+//		System.out.println("Iteration ..... the \"new-style\" way");
+//		while (i.hasNext())
+//			System.out.println(i.next());
+	//}
+}
